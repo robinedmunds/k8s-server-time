c24cab7108791cf29001f995881d3eab0844c093
Robin Edmunds <8656189+robinedmunds@users.noreply.github.com>
2021-06-28 16:54:25 +0100

    Add express api server

---
+++ src/server/src/server.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const app = express()
+const port = 3000
+
+app.get("/", (req, res) => {
+    res.send({
+        api: {
+            message: "The express api is responding correctly..."
+        }
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Express server running on port ${port} ...`)
+})
