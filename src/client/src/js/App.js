f9112d178276f4edb259bfc1c556ceb8a2d9956e
Robin Edmunds <8656189+robinedmunds@users.noreply.github.com>
2021-06-29 14:47:13 +0100

    Add Time component. Styling

--- src/client/src/js/App.js
+++ src/client/src/js/App.js
@@ -1,11 +1,16 @@
 import React from "react"
 import "sanitize.css"
 import "../styles/App.scss"
+import Time from "./components/Time"
 
 const App = () => {
   return (
-    <div>
-      <h1>React: App.js</h1>
+    <div class="container">
+      <div class="block">
+        <h1>Server Time</h1>
+        <Time />
+        <p>This is a two docker image Kubernetes demo. It comprises of a parcel, react, nginx, frontend. And a node, express backend json api which supplies the time.</p>
+      </div>
     </div>
   )
 }
---
+++ src/client/src/js/components/Time.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Time = () => {
+  return (
+    <div class="time">99:99:99</div>
+  )
+}
+
+export default Time
