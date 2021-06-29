baabd77215a779d50140c435eb64b5a70421e074
Robin Edmunds <8656189+robinedmunds@users.noreply.github.com>
2021-06-29 14:59:13 +0100

    Minor style tweaks

--- src/client/src/js/App.js
+++ src/client/src/js/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component } from "react"
 import "sanitize.css"
 import "../styles/App.scss"
 import Time from "./components/Time"
@@ -10,7 +10,7 @@ class App extends Component {
     return (
       <div class="container">
         <div class="block">
-          <h1>Server Time</h1>
+          <h1>API Server Time</h1>
           <Time formatedTime={formatedTime} />
           <p>This is a two docker image Kubernetes demo. It comprises of a parcel, react, nginx, frontend. And a node, express backend json api which supplies the time.</p>
         </div>
