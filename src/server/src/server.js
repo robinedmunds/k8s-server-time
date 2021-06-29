b61274d612b292d2cae18d95013ac4b95f01fe38
Robin Edmunds <8656189+robinedmunds@users.noreply.github.com>
2021-06-29 12:08:55 +0100

    Change 4 to 2 space indendation

--- src/server/src/server.js
+++ src/server/src/server.js
@@ -3,13 +3,13 @@ const app = express()
 const port = 3000
 
 app.get("/", (req, res) => {
-    res.send({
-        api: {
-            message: "The express api is responding correctly..."
-        }
-    })
+  res.send({
+    api: {
+      message: "The express api is responding correctly..."
+    }
+  })
 })
 
 app.listen(port, () => {
-    console.log(`Express server running on port ${port} ...`)
+  console.log(`Express server running on port ${port} ...`)
 })
