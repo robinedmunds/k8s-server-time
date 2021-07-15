83054ab4034ac6658ec8356e4eab2e64eb7e07e5
Robin Edmunds <8656189+robinedmunds@users.noreply.github.com>
2021-07-15 20:27:21 +0100

    Move .env files, .env files utilised in client

--- src/client/src/js/api/getServerTime.js
+++ src/client/src/js/api/getServerTime.js
@@ -1,8 +1,11 @@
 import axios from "axios"
 
 export default async () => {
-  const apiURL = process.env.API_URL || "/"
-  const apiPort = `:${process.env.apiPort}` || ""
+  const apiURL = process.env.API_URL || ""
+  let apiPort = ""
+  if (process.env.PORT) {
+    apiPort = `:${process.env.PORT}`
+  } 
   const response = await axios.get(`${apiURL}${apiPort}`)
   const datetime = new Date(response.data.data.datetime)
   return datetime
